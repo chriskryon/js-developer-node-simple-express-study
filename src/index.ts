b65b6637c98bed8b400259b83e77d14cfeee9b23
Christopher <contato.christopher@gmail.com>
2022-11-20 12:57:22 -0300

    add post, put and delete

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express, { Request, Response, NextFunction } from "express";
+import usersRoute from "./routes/user.routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(usersRoute);
 
 app.use("/status", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: "bar" });
