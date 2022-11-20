a53b43ce1582f49c864cb4992b8a45eff6aeb09b
Christopher <contato.christopher@gmail.com>
2022-11-20 11:28:46 -0300

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import express, { Request, Response, NextFunction } from "express";
 
-console.log("Executei");
+const app = express();
+
+app.use("/status", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: "bar" });
+});
+
+app.listen(3000, () => {
+    console.log("Aplicação executando na porta 3000");
+})
